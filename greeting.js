6bb0d28b12372a3f29d30017e42a57b137db5ce5
skykchmin <skykchmin@naver.com>
2020-07-12 20:40:37 +0900

    [200712] 이름 저장 및 입력
    [200712] 이름 저장 및 입력

--- greeting.js
+++ greeting.js
@@ -5,13 +5,26 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
 SHOWING_CN ="showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+
+function handleSubmit(event){ 
+    event.preventDefault(); //event가 발생하면 페이지가 새로고침 되는데 새로고침 되는 이벤트를 막는다
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
 function askForName(){ //user가 없으면 이름을 요청한다.
     form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit); //form을 처리
 }
 
 function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
+    form.classList.remove(SHOWING_CN); // form을 지우고
+    greeting.classList.add(SHOWING_CN); // greeting을 보여준다
     greeting.innerText = `Hello ${text}`;
     
 }
